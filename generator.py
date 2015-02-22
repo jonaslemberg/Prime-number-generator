219147424f0d329e0b10bd01b89731b28b3378c7
Jonas Lemberg <jonas.lemberg@aalto.fi>
2015-02-22 18:53:08 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,9 +1,13 @@
 import math
 
 def isnumeric(i):
+
+	# checks if the input is numeric
     return all(char in "0123456789.-+" for char in i)
 
 def output(primes, size):
+
+	# prints the list of prime numbers in a 10 column table
     row_count = 0
     r = ""
     
@@ -26,24 +30,28 @@ def main():
     x = "a"
     y = "b"
 
+	#check if input is numeric
     while isnumeric(x) == False:
         x = input("Enter a number: ")
     
     while isnumeric(y) == False:
         y = input("Enter another number: ")
     
+	# save the original numbers for later printing
     x_old = x
     y_old = y
-
+	
     x = math.ceil(float(x))
     y = math.trunc(float(y))
     
+	# if any of the given integers are negative, start generating numbers starting from 0, since there are no negative prime numbers.
     if x < 0:
         x = 0
     
     if y < 0:
         y = 0
     
+	# if the second number given is smaller than the first number, the variable add gets a negative value
     if y < x:
         add = -1
         size = len(str(x))
@@ -53,7 +61,7 @@ def main():
         
     print("\nPrime numbers in the range {:s}".format(x_old),"- {:s}:\n".format(y_old))
 
-    diff = abs(y - x)
+	diff = abs(y - x) #the difference between the two given numbers
     primes = []
     
     for i in range (0, diff+1):
@@ -66,7 +74,7 @@ def main():
             elif div == num:
                 primes.append(num)
             div += 1
-        
-    output(primes, size)
+    
+    output(primes, size) #print the table
     
 main()
